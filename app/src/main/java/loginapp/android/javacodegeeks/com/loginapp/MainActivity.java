30552a7a53c9a7cbe24162fe1ea1ba043574bb52
sayom88 <sayom88@yahoo.com>
2016-01-20 11:36:22 +0530

    20th Jan-Code checkin-4

--- app/src/main/java/loginapp/android/javacodegeeks/com/loginapp/MainActivity.java
+++ app/src/main/java/loginapp/android/javacodegeeks/com/loginapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView loginLockedTV;
     private TextView attemptsLeftTV;
     private TextView numberOfRemainingLoginAttemptsTV;
-    int numberOfRemainingLoginAttempts = 4;
+    int numberOfRemainingLoginAttempts = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
